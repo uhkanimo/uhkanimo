d2efb3d3d841019b1bfb1df2b922fda45592a20f
uhkanimo <akanimoeshiet8@gmail.com>
2025-11-18 13:42:09 -0600

    made project foler

---
+++ Fitness Tracker Project/Fitness Tracker Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Fitness Tracker Project
+//
+//  Created by Akanimo Eshiet II on 11/18/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
